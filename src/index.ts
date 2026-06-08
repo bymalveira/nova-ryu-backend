9cd8c1aa9d43ce0360d1c23d51646ceb803d7c11
bymalveira <marcusmalveira163@gmail.com>
2026-06-08 18:28:35 -0300

    fix: url mudada para localhost:4000 e crud autorizado de outras urls.

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,20 @@ import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { userRoutes } from './routes/user.routes';
 import adminRoutes from './routes/admin.routes';
+import { cors } from 'hono/cors';
 
 const app = new Hono();
+
+app.use('/*', cors({
+    origin: 'http://localhost:3000',
+    allowMethods: ['GET', 'POST', 'DELETE', 'PUT'],
+    allowHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.route('/users', userRoutes);
 app.route('/admin', adminRoutes);
 
-serve(app);
+serve({
+    fetch: app.fetch,
+    port: 4000
+});
